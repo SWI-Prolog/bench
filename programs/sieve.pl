863e1ccbe5c2f667b07926cd34cb1bb5a6c6cd64
Jan Wielemaker <J.Wielemaker@vu.nl>
2023-11-22 21:41:42 +0100

    PORT: sieve.pl: avoid forall/2, ignore/2 and between/3.

--- programs/sieve.pl
+++ programs/sieve.pl
@@ -8,16 +8,6 @@
 top :- clean, primes(10000), fail.
 top.
 
-:- if(\+predicate_property(forall(_,_), _)).
-forall(Cond,Act) :- \+ ( Cond, \+ Act ).
-:- endif.
-:- if(\+predicate_property(ignore(_), _)).
-ignore(Goal) :- (Goal->true;true).
-:- endif.
-:- if(\+predicate_property(between(_,_,_),_)).
-:- use_module(library(between)).
-:- endif.
-
 :- dynamic prime/1, candidate/1.
 
 clean :-
@@ -25,7 +15,7 @@ clean :-
     retractall(candidate(_)).
 
 primes(N) :-
-    forall(between(2, N, I), assertz(candidate(I))),
+    \+ ( range(2, N, I), \+ assertz(candidate(I))),
     sieve(N).
 
 sieve(Max) :-
@@ -41,7 +31,19 @@ sieve(N, Mul, Max) :-
     I is N*Mul,
     I =< Max,
     !,
-    ignore(retract(candidate(I))),
+    (   retract(candidate(I))
+    ->  true
+    ;   true
+    ),
     Mul2 is Mul+1,
     sieve(N, Mul2, Max).
 sieve(_, _, _).
+
+% is between/3, but only in mode +,+,-.  Simple Prolog definition
+% for portability.
+range(Low, High, Low) :-
+    Low =< High.
+range(Low, High, I) :-
+    Low2 is Low+1,
+    Low2 =< High,
+    range(Low2, High, I).
